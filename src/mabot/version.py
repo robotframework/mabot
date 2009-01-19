1b9be4eb2e9cfe7120f257a5bb6693c5e97a6dde
jprantan <jprantan@f6fba404-a43b-11dd-9bad-6bc74a82fd44>
2009-01-19 09:17:03 +0000

    Changed version to trunk.
    git-svn-id: https://robotframework-mabot.googlecode.com/svn/trunk@45 f6fba404-a43b-11dd-9bad-6bc74a82fd44

--- src/mabot/version.py
+++ src/mabot/version.py
@@ -12,5 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import time
 
-version = "0.5"
+version = "trunk-%d%02d%02d" % (time.localtime()[:3])
